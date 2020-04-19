c8e87fabec709c0755f42107f4493cce69a2647c
yashrao99 <yashas@suki.ai>
2020-04-18 18:14:04 -0700

    Initial Commit

---
+++ CodeDesignApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CodeDesignApp
+//
+//  Created by Yashas Rao on 4/18/20.
+//  Copyright © 2020 CodeDesign. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
