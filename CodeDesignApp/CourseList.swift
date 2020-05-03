e652feeb49556060e7fd7d17685e2a58df11b6ee
yashrao99 <yashas@suki.ai>
2020-05-02 18:04:03 -0700

    Uses data model array rather than define states in each object in scroll biew, uses geometry reader frame to NOT push other objects, but rather take over the entire screen without changing other object position

--- CodeDesignApp/CourseList.swift
+++ CodeDesignApp/CourseList.swift
@@ -9,24 +9,32 @@
 import SwiftUI
 
 struct CourseList: View {
-    // This currently uses 2 states, which will be changed in the next section
-    @State var show = false
-    @State var show2 = false
+    @State var courses = courseData
+    
     var body: some View {
         ScrollView {
             VStack(spacing: 30) {
-                CourseView(show: $show)
+                Text("Courses")
+                    .font(.largeTitle).bold()
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.leading, 30)
+                    .padding(.top, 30)
+                    
                 // This geometry reader allows us to take the second CourseView object and use it to animate this view to the top when pressed. We use the show2 state, so when pressed and it gets toggled. That's why it is a binding (show) in the courseView component. that's how it toggles the variable in the CourseList parent view.
-                GeometryReader { geometry in
-                    CourseView(show: self.$show2)
-                        // Animates the cell to the top, when pressed, or keeps it where iti s
-                        .offset(y: self.show2 ? -geometry.frame(in: .global).minY : 0)
+                ForEach(courses.indices, id: \.self) { index in
+                    GeometryReader { geometry in
+                        CourseView(show: self.$courses[index].show, course: self.courses[index])
+                            // Animates the cell to the top, when pressed, or keeps it where iti s
+                            .offset(y: self.courses[index].show ? -geometry.frame(in: .global).minY : 0)
+                    }
+                        // Geometry reader pushes the other objects in the scroll View. This is fine if you're okay with it. But if not, manually set the frame here. In the actual CourseView object, set the frame to trigger on the outer most containerView to adapt to the full screen based on the state of Show in the dataModel. However, the z-stack needs to be fixed so that the expanded courseView takes over the screen completely and you don't see the other components
+                    .frame(height: 280)
+                        // When using geometry reader, the padding gets ignored. So we have to manually set it to screen.width - 60 so it looks identical to the first CourseList when not animated out.
+                        .frame(maxWidth: self.courses[index].show ? .infinity : screen.width - 60)
                 }
-                .frame(height: show2 ? screen.height : 280)
-                    // When using geometry reader, the padding gets ignored. So we have to manually set it to screen.width - 60 so it looks identical to the first CourseList when not animated out.
-                .frame(maxWidth: show2 ? .infinity : screen.width - 60)
             }
             .frame(width: screen.width)
+            .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
         }
     }
 }
@@ -38,7 +46,10 @@ struct CourseList_Previews: PreviewProvider {
 }
 
 struct CourseView: View {
+
     @Binding var show: Bool
+    var course: Course
+    
     var body: some View {
         // This Z-stack allows us to have content underneath the existing card view. This aligns whichever component in the Z-stack to the top, and to the biggest component
         ZStack(alignment: .top) {
@@ -63,17 +74,17 @@ struct CourseView: View {
                 HStack(alignment: .top) {
                     // 8 pixels stacked on top of each other, aligned to leading
                     VStack(alignment: .leading, spacing: 8) {
-                        Text("Swift UI Advanced")
+                        Text(course.title)
                             .font(.system(size: 24, weight: .bold))
                             .foregroundColor(Color.white)
-                        Text("20 sections")
+                        Text(course.subtitle)
                             .foregroundColor(Color.white.opacity(0.7))
                     }
                     // spacer forces the image and the HStack takes the max width between the frame width
                     Spacer()
                     // This Z-stack puts two buttons on top of each other. Based on the state, will change the button
                     ZStack {
-                        Image(uiImage: #imageLiteral(resourceName: "Logo1"))
+                        Image(uiImage: course.logo)
                             .opacity(show ? 0 : 1)
                         VStack {
                             Image(systemName: "xmark")
@@ -88,7 +99,7 @@ struct CourseView: View {
                 }
                 // This pushes the HStack to the top, and the other image down to the bottom
                 Spacer()
-                Image(uiImage: #imageLiteral(resourceName: "Card6"))
+                Image(uiImage: course.image)
                     .resizable()
                     .aspectRatio(contentMode: .fit)
                     .frame(maxWidth: .infinity)
@@ -99,18 +110,36 @@ struct CourseView: View {
             .padding(.top, show ? 30 : 0)
                 // This forces the view, when show, to take the maximum height and width (while respecting the safearealayout)
             .frame(maxWidth: show ? .infinity : screen.width - 60, maxHeight: show ? 460 : 280)
-            .background(Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)))
+                .background(Color(course.color))
                 // Creates the smooth rounded corners
             .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
-            .shadow(color: Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)).opacity(0.3), radius: 20, x: 0, y: 20)
+                .shadow(color: Color(course.color).opacity(0.3), radius: 20, x: 0, y: 20)
             .onTapGesture {
                 // toggles the state when tapped
                 self.show.toggle()
             }
         }
+        .frame(height: course.show ? screen.height : 280)
             // We set the frame based on the show state, and this animation kicks in
         .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
         // This forces us to ignore safearealayout, stretches to max
         .edgesIgnoringSafeArea(.all)
     }
 }
+
+// Creates the view data-model for the courseList so we can loop through rather than store a bunch of states for each data component
+struct Course: Identifiable {
+       var id = UUID()
+       var title: String
+       var subtitle: String
+       var image: UIImage
+       var logo: UIImage
+       var color: UIColor
+       var show: Bool
+}
+
+var courseData = [
+   Course(title: "Prototype Designs in SwiftUI", subtitle: "18 Sections", image: #imageLiteral(resourceName: "Background1"), logo: #imageLiteral(resourceName: "Logo1"), color: #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1), show: false),
+   Course(title: "SwiftUI Advanced", subtitle: "20 Sections", image: #imageLiteral(resourceName: "Card3"), logo: #imageLiteral(resourceName: "Logo1"), color: #colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1), show: false),
+   Course(title: "UI Design for Developers", subtitle: "20 Sections", image: #imageLiteral(resourceName: "Card4"), logo: #imageLiteral(resourceName: "Logo3"), color: #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1), show: false)
+]
